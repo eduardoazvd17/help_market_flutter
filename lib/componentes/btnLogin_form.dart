9f5c375751ffdde284522d649ef769771f3bd844
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 18:15:10 -0300

    Inkwell fix.

--- lib/componentes/btnLogin_form.dart
+++ lib/componentes/btnLogin_form.dart
@@ -12,19 +12,21 @@ class BtnLoginForm extends StatelessWidget {
     this.width,
     @required this.funcao,
   });
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.053,
-      width: width,
-      padding: EdgeInsets.only(
-        left: 12,
-        right: 12,
-      ),
-      decoration:
-          BoxDecoration(color: cor, borderRadius: BorderRadius.circular(50)),
-      child: InkWell(
-        onTap: funcao,
+    return InkWell(
+      borderRadius: BorderRadius.circular(50),
+      onTap: funcao,
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.053,
+        width: width,
+        padding: EdgeInsets.only(
+          left: 12,
+          right: 12,
+        ),
+        decoration:
+            BoxDecoration(color: cor, borderRadius: BorderRadius.circular(50)),
         child: Center(
           child: Text(
             texto,
