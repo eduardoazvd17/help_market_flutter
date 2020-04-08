2ec4ee9b9016c30aecb6b8dc358e416541254a9e
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 16:33:25 -0300

    finalizando previamente tela cadastro.

--- lib/telas/tela_cadastro.dart
+++ lib/telas/tela_cadastro.dart
@@ -1,13 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:lista_compras/componentes/btnLogin_form.dart';
+import 'package:lista_compras/componentes/inputLogin_form.dart';
+import 'package:lista_compras/telas/tela_cadastro.dart';
 
 class TelaCadastro extends StatelessWidget {
+  var nomeController = TextEditingController();
+  var userController = TextEditingController();
+  var passController = TextEditingController();
+  var confirmPassController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var altura = MediaQuery.of(context).size.height;
     var largura = MediaQuery.of(context).size.width;
 
-    Container(
-      child: Text("Teste"),
+    return SingleChildScrollView(
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              // color: Colors.purple,
+              width: largura * 0.98,
+              height: altura * 0.82,
+              child: Card(
+                elevation: 10,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: altura * 0.005,
+                      ),
+                      child: Text(
+                        "Seja Bem-Vindo!",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: altura * 0.005),
+                      child: Text(
+                        "Cadastre-se para continuar.",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    InputLoginForm(
+                      icon: Icon(Icons.person),
+                      controller: nomeController,
+                      hintText: "Nome",
+                    ),
+                    InputLoginForm(
+                      icon: Icon(Icons.person),
+                      controller: userController,
+                      hintText: "Email",
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    InputLoginForm(
+                      icon: Icon(Icons.lock),
+                      controller: passController,
+                      hintText: "Senha",
+                      obscureText: true,
+                    ),
+                    InputLoginForm(
+                      icon: Icon(Icons.lock),
+                      controller: confirmPassController,
+                      hintText: "Confirmar Senha",
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: altura * 0.02,
+                    ),
+                    BtnLoginForm(
+                      width: largura * 0.858,
+                      texto: "Cadastrar-se",
+                      funcao: () {},
+                      cor: Colors.green,
+                    ),
+                    SizedBox(
+                      height: altura * 0.02,
+                    ),
+                    BtnLoginForm(
+                      width: largura * 0.858,
+                      texto: "Cancelar",
+                      cor: Colors.red,
+                      funcao: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
