35f92798a82549a3c744911f21a55775b54f0fb8
Eduardo Azevedo <eduardoazvd17@gmail.com>
2020-04-08 08:24:59 -0300

    Commit Inicial

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ListaCompras());
+
+class ListaCompras extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
