28244bb11d0f7f2e13b5f662e8c24bbc437c57a4
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 17:12:53 -0300

    criando visual para o Drawer.

--- lib/componentes/main_drawer.dart
+++ lib/componentes/main_drawer.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:lista_compras/componentes/listTile_inkWell.dart';
 
 class MainDrawer extends StatelessWidget {
+  final String nome;
+  final String email;
+
+  MainDrawer({
+    this.email,
+    this.nome,
+  });
+
+  //TODO: adicionar funcionalidade aos botoes do drawer.
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -8,7 +19,56 @@ class MainDrawer extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: Container(),
+            child: Container(
+              width: double.infinity,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: ThemeData().primaryColor,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.people,
+                      size: MediaQuery.of(context).size.height * 0.1,
+                    ),
+                    Text(
+                      "Desenvolvedor",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "dev@development.com",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                Divider(),
+                ListTileInkWell(
+                  icon: Icon(Icons.settings),
+                  text: "Configurações",
+                  function: () {},
+                ),
+                Divider(),
+                ListTileInkWell(
+                  text: "Sobre-nós",
+                  function: () {},
+                ),
+                ListTileInkWell(
+                  text: "Privacidade",
+                  function: () {},
+                ),
+              ],
+            ),
           ),
         ],
       ),
