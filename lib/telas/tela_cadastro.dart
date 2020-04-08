37060f9ef873bb5d2823e18b3855fd607fadb3dd
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 14:18:21 -0300

    criando tela de cadastro.

---
+++ lib/telas/tela_cadastro.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var altura = MediaQuery.of(context).size.height;
+    var largura = MediaQuery.of(context).size.width;
+
+    Container(
+      child: Text("Teste"),
+    );
+  }
+}
