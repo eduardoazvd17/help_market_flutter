7f5e8fd15c01cb8b7565e6f52ba43a34a0050b7f
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 11:42:04 -0300

    adicionando um tema.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ class ListaCompras extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
       debugShowCheckedModeBanner: false,
       home: TelaInicio(),
     );
