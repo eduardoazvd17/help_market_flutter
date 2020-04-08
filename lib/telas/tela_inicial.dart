f9265ed4d428d58179317d276015d2ac33273d12
Eduardo Azevedo <eduardoazvd17@gmail.com>
2020-04-08 12:12:17 -0300

    Criação do bottom navigation bar

---
+++ lib/telas/tela_inicial.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TelaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/telas/tela_inicio.dart
+++ lib/telas/tela_inicio.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:lista_compras/telas/tela_configuracoes.dart';
+import 'package:lista_compras/telas/tela_inicial.dart';
+import 'package:lista_compras/telas/tela_listas.dart';
 
-class TelaInicio extends StatelessWidget {
+class TelaInicio extends StatefulWidget {
+  final List<Widget> _telas = [
+    TelaInicial(),
+    TelaListas(),
+    TelaConfiguracoes(),
+  ];
+
+  @override
+  _TelaInicioState createState() => _TelaInicioState();
+}
+
+class _TelaInicioState extends State<TelaInicio> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inicio'),
+        title: Text(
+          _selectedIndex == 0
+              ? 'Inicio'
+              : _selectedIndex == 1 ? 'Minhas Listas' : 'Configurações',
+        ),
       ),
-      body: Container(),
+      body: widget._telas[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Inicio'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Minhas Listas'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Configurações'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
