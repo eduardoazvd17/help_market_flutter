9b8e3f59bc79d761d3287466d355febc42ee4ab8
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 18:35:12 -0300

    adicionando efeito visual na tela login.

--- lib/telas/tela_inicial.dart
+++ lib/telas/tela_inicial.dart
@@ -14,57 +14,67 @@ class TelaInicial extends StatelessWidget {
 
     return SingleChildScrollView(
       child: Container(
-        margin: EdgeInsets.only(top: altura * 0.015),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: altura * 0.25,
-              child: Image.network(
-                "https://cdn.pixabay.com/photo/2018/10/03/21/44/shopping-3722450_960_720.png",
-              ),
+        color: Theme.of(context).primaryColor,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              topRight: Radius.circular(40.0),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: altura * 0.02,
+          ),
+          margin: EdgeInsets.only(top: altura * 0.015),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: altura * 0.25,
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2018/10/03/21/44/shopping-3722450_960_720.png",
+                ),
               ),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Welcome \$user!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: altura * 0.02,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Welcome \$user!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: altura * 0.015,
-                  ),
-                  Text(
-                    "Welcome \$user! Welcome \$user! Welcome \$user! ",
-                    style: TextStyle(),
-                  ),
-                ],
+                    SizedBox(
+                      height: altura * 0.015,
+                    ),
+                    Text(
+                      "Welcome \$user! Welcome \$user! Welcome \$user! ",
+                      style: TextStyle(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: largura * 0.02,
-                vertical: altura * 0.01,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: largura * 0.02,
+                  vertical: altura * 0.01,
+                ),
+                child: LoginForm(
+                  passController: usuarioCtrl,
+                  userController: senhaCtrl,
+                ),
               ),
-              child: LoginForm(
-                passController: usuarioCtrl,
-                userController: senhaCtrl,
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Não tem uma conta? REGISTRE-SE",
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
               ),
-            ),
-            FlatButton(
-              onPressed: () {},
-              child: Text(
-                "Não tem uma conta? REGISTRE-SE",
-                style: TextStyle(color: Theme.of(context).primaryColor),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
