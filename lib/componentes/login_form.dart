d290ca8b608d2616b4d6f3a191bb6b90fa91be03
MateusHBR <mateusmorais.r@gmail.com>
2020-04-08 13:05:43 -0300

    ajustes visuais no loginform.

--- lib/componentes/login_form.dart
+++ lib/componentes/login_form.dart
@@ -15,67 +15,67 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       // padding: EdgeInsets.symmetric(horizontal: 5),
-        child: Container(
+      child: Container(
         height: MediaQuery.of(context).size.height * 0.30,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.indigo,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            children: <Widget>[
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: <Widget>[
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.015,
             ),
-              InputLoginForm(
+            InputLoginForm(
               icon: Icon(Icons.person),
-                controller: userController,
+              controller: userController,
               hintText: "Usuario",
-              ),
-              InputLoginForm(
+            ),
+            InputLoginForm(
               icon: Icon(Icons.lock),
-                controller: passController,
+              controller: passController,
               hintText: "Senha",
-              ),
-              Padding(
+            ),
+            Padding(
               padding: EdgeInsets.only(top: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    FlatButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Esqueceu a senha?",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                      left: 12,
+                      right: 12,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.indigo[300],
+                        borderRadius: BorderRadius.circular(50)),
+                    child: FlatButton(
                       onPressed: () {},
                       child: Text(
-                      "Esqueceu a senha?",
+                        "LOGAR",
                         style: TextStyle(
                           color: Colors.white,
+                          fontSize: 16,
                         ),
                       ),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(
-                        left: 12,
-                        right: 12,
-                      ),
-                      decoration: BoxDecoration(
-                          color: Colors.indigo[300],
-                          borderRadius: BorderRadius.circular(50)),
-                      child: FlatButton(
-                        onPressed: () {},
-                        child: Text(
-                        "LOGAR",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
+      ),
     );
   }
 }
