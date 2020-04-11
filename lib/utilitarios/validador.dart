2bfb264d4b7dfebda518ba6df0de9487dbdb8a0b
MateusHBR <mateusmorais.r@gmail.com>
2020-04-11 13:07:44 -0300

    removendo import não utilizado.

--- lib/utilitarios/validador.dart
+++ lib/utilitarios/validador.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lista_compras/modelos/usuario.dart';
-import 'package:lista_compras/telas/tela_inicio.dart';
 
 class Validador {
   BuildContext context;
